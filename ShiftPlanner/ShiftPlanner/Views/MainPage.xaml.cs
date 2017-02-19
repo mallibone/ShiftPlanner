93c3e99eb35c7999711270ba981e1b943e9a533a
Mark <mallibone@outlook.com>
2017-02-19 11:40:20 +0100

    Improve button styling on iOS

--- ShiftPlanner/ShiftPlanner/Views/MainPage.xaml.cs
+++ ShiftPlanner/ShiftPlanner/Views/MainPage.xaml.cs
@@ -19,5 +19,12 @@ namespace ShiftPlanner.Views
         }
 
         private MainViewModel Vm => App.Locator.MainViewModel;
+
+        void Handle_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null) return;
+            MonthlyShifts.SelectedItem = null;
+        }
+
     }
 }
