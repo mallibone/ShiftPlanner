e44c576cb98ef2c283d3d2e8e19f7ab12493a1fd
mallibone <mallibone@outlook.com>
2017-04-23 10:02:44 +0200

    Update NuGet packages and add .bak to ignore file

--- ShiftPlanner/ShiftPlanner/Services/ShiftService.cs
+++ ShiftPlanner/ShiftPlanner/Services/ShiftService.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ShiftPlanner.Models;
 using ShiftPlanner.Utils;
+using ShiftPlanner.ViewModels;
 
 namespace ShiftPlanner.Services
 {
     class ShiftService
     {
+        private readonly IRepository _repository;
         private ICollection<ShiftType> _shiftTypes;
 
-        public ShiftService()
+        public ShiftService(IRepository repository)
         {
+            if (repository == null) throw new ArgumentNullException(nameof(repository));
+            _repository = repository;
+
             _shiftTypes = new List<ShiftType>
             {
                 new ShiftType {Id = 0, Name = "Day Off" },
@@ -28,6 +31,50 @@ namespace ShiftPlanner.Services
         public IEnumerable<ShiftType> GetShiftTypes()
         {
             return _shiftTypes.Clone();
+        }
+
+        public IEnumerable<DayViewModel> GetMonth(DateTime monthRequested)
+        {
+            var monthShifts = _repository.GetShiftsForMonth(monthRequested);
+            return monthShifts.Any() ? monthShifts : StubInit(monthRequested);
+        }
+        private IEnumerable<DayViewModel> StubInit(DateTime offsetDate)
+        {
+            var daysInCurrentMonth = DateTime.DaysInMonth(offsetDate.Year, offsetDate.Month);
+            var daysInMonth = new DayViewModel[daysInCurrentMonth];
+
+            for (int i = 0; i < daysInCurrentMonth; ++i)
+            {
+                daysInMonth[i] = new DayViewModel
+                {
+                    Date = new DateTime(offsetDate.Year, offsetDate.Month, i + 1),
+                    SelectedShiftType = DefaultShift,
+                };
+            }
+
+            return daysInMonth;
+        }
+    }
+
+    internal interface IRepository
+    {
+        IEnumerable<Shift> GetShiftsForMonth(DateTime monthRequested);
+    }
+
+    internal class Shift
+    {
+        public int Id { get; set; }
+        public DateTime DateChanged { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime Date { get; set; }
+        public ShiftType SelectedShiftType { get; set; }
+    }
+
+    internal class StubRepository:IRepository
+    {
+        public IEnumerable<Shift> GetShiftsForMonth(DateTime monthRequested)
+        {
+            throw new NotImplementedException();
         }
     }
 }
